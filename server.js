12cbf5bdfed2d01b595b0eecf344cc33e7e99301
volik <volik.anton@gmail.com>
2020-08-07 18:23:22 +0300

    add public dir

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('./'));
+app.use(express.static('./public'));
 
 app.get('/video', (req, res) => {
-    res.download('./sintel.mp4');
+    res.download('./public/sintel.mp4');
   })
 
 app.listen(port, () => {
